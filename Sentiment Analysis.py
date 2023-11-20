872e18d95ded757edd953d7355edd358b00ee61f
Grashal Vishwa <89205098+vgrashal16@users.noreply.github.com>
2023-11-20 10:12:49 +0530

    Add files via upload

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -1,66 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## Sentiment Analysis on US Airline Reviews
-
-# In[1]:
-
-
-import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
 import matplotlib.pyplot as plt
-
-from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pandas as pd
+from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM,Dense, Dropout, SpatialDropout1D
-from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import LSTM, Dense, Dropout, SpatialDropout1D, Embedding
 
 df = pd.read_csv("./Tweets.csv")
-
-
-# In[2]:
-
-
-df.head()
-
-
-# In[23]:
-
-
-df.columns
-
-
-# In[4]:
-
-
-tweet_df = df[['text','airline_sentiment']]
-print(tweet_df.shape)
-tweet_df.head(5)
-
-
-# In[22]:
-
-
+tweet_df = df[['text', 'airline_sentiment']]
 tweet_df = tweet_df[tweet_df['airline_sentiment'] != 'neutral']
-print(tweet_df.shape)
-tweet_df.head(5)
-
-
-# In[21]:
-
-
-tweet_df["airline_sentiment"].value_counts()
-
-
-# In[6]:
-
 
 sentiment_label = tweet_df.airline_sentiment.factorize()
-sentiment_label
-
-
-# In[7]:
-
 
 tweet = tweet_df.text.values
 tokenizer = Tokenizer(num_words=5000)
@@ -70,87 +21,56 @@ encoded_docs = tokenizer.texts_to_sequences(tweet)
 padded_sequence = pad_sequences(encoded_docs, maxlen=200)
 
 
-# In[8]:
-
-
-print(tokenizer.word_index)
-
-
-# In[9]:
-
-
-print(tweet[0])
-print(encoded_docs[0])
-
-
-# In[10]:
-
-
-print(padded_sequence[0])
-
-
-# In[11]:
-
-
 embedding_vector_length = 32
-model = Sequential() 
-model.add(Embedding(vocab_size, embedding_vector_length, input_length=200) )
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_vector_length, input_length=200))
 model.add(SpatialDropout1D(0.25))
 model.add(LSTM(50, dropout=0.5, recurrent_dropout=0.5))
 model.add(Dropout(0.2))
-model.add(Dense(1, activation='sigmoid')) 
-model.compile(loss='binary_crossentropy',optimizer='adam', metrics=['accuracy'])  
-print(model.summary()) 
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+history = model.fit(padded_sequence, sentiment_label[0], validation_split=0.2, epochs=5, batch_size=32)
+print(history.history.keys())
 
-# In[12]:
-
-
-history = model.fit(padded_sequence,sentiment_label[0],validation_split=0.2, epochs=5, batch_size=32)
-
-
-# In[16]:
-
-
-plt.plot(history.history['accuracy'], label='acc')
-plt.plot(history.history['val_accuracy'], label='val_acc')
-plt.legend()
-plt.show()
-plt.savefig("Accuracy plot.jpg")
-
-
-# In[25]:
-
-
-plt.plot(history.history['loss'], label='loss')
-plt.plot(history.history['val_loss'], label='val_loss')
-plt.legend()
-plt.show()
-plt.savefig("Loss plot.jpg")
-
-
-# In[18]:
-
-
-def predict_sentiment(text):
+def predict_sentiment():
+    text = user_input.get()
+    if text.strip() == "":
+        messagebox.showerror("Error", "Please enter a sentence.")
+        return
+    
     tw = tokenizer.texts_to_sequences([text])
-    tw = pad_sequences(tw,maxlen=200)
+    tw = pad_sequences(tw, maxlen=200)
     prediction = int(model.predict(tw).round().item())
-    print("Predicted label: ", sentiment_label[1][prediction])
+    predicted_label = sentiment_label[1][prediction]
+    messagebox.showinfo("Prediction", f"Predicted label: {predicted_label}")
 
+def plot():
+    plt.plot(history.history['accuracy'], label='acc')
+    plt.plot(history.history['val_accuracy'], label='val_acc')
+    plt.legend()
+    plt.show()
+    plt.savefig("Accuracy plot.jpg")
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.legend()
+    plt.show()
+    plt.savefig("Loss plot.jpg")
 
-# In[19]:
+root = tk.Tk()
+root.title("Sentiment Analysis")
+root.geometry("400x250")
 
+input_label = tk.Label(root, text="Enter a sentence:")
+input_label.pack()
 
-test_sentence1 = "I enjoyed my journey on this flight."
-predict_sentiment(test_sentence1)
+user_input = tk.Entry(root, width=50)
+user_input.pack()
 
-test_sentence2 = "This is the worst flight experience of my life!"
-predict_sentiment(test_sentence2)
+predict_button = tk.Button(root, text="Predict", command=predict_sentiment)
+predict_button.pack()
 
+accuracy_button = tk.Button(root, text="Show Plots", command=plot)
+accuracy_button.pack()
 
-# In[ ]:
-
-
-
-
+root.mainloop()
